c4434ea5a7519aa9abeb980607336033a7b1d507
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-10-06 22:00:07 +0100

    first commit

--- URLShortner/src/main/java/com/UrlShortner/util/KeyGenerator.java
+++ URLShortner/src/main/java/com/UrlShortner/util/KeyGenerator.java
@@ -1,5 +1,18 @@
 package com.UrlShortner.util;
 
+import java.security.SecureRandom;
+
 public class KeyGenerator {
     private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int LENGTH = 6;
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generateKey(){
+        StringBuilder builder = new StringBuilder(LENGTH);
+        for(int count = 0; count < LENGTH; count++){
+            int index = random.nextInt(CHARACTERS.length());
+            builder.append(CHARACTERS.charAt(index));
+        }
+        return builder.toString();
+    }
 }
