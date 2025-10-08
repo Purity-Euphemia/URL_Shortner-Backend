7b6d37e3b876d8d6e2716d4feb4133f9d80e0150
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-10-08 03:31:40 +0100

    first commit

--- URLShortner/src/main/java/com/UrlShortner/service/UserService.java
+++ URLShortner/src/main/java/com/UrlShortner/service/UserService.java
@@ -1,12 +1,16 @@
 package com.UrlShortner.service;
 
+import com.UrlShortner.data.model.User;
 import com.UrlShortner.data.repository.UserRepository;
-import org.springframework.security.core.userdetails.*;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService implements UserDetailsService{
+public class UserService implements UserDetailsService {
 
     private final UserRepository repository;
     private final PasswordEncoder encoder;
@@ -15,13 +19,24 @@ public class UserService implements UserDetailsService{
         this.repository = repository;
         this.encoder = encoder;
     }
-    public void register(User user){
-        if(repository.existsByUsername(user.getUsername())){
+
+    public void register(User user) {
+        if (repository.existsByUsername(user.getUsername())) {
             throw new RuntimeException("Username already exists");
         }
         user.setPassword(encoder.encode(user.getPassword()));
         repository.save(user);
     }
+
     @Override
-    public 
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
 }
