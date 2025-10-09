3465ee10e7030ceb030576f5e4f0e5e42d38c863
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-10-09 02:42:52 +0100

    first commit

--- URLShortner/src/main/java/com/UrlShortner/config/CorsConfig.java
+++ URLShortner/src/main/java/com/UrlShortner/config/CorsConfig.java
@@ -14,8 +14,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://127.0.0.1:5500")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-                
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+
             }
         };
     }
